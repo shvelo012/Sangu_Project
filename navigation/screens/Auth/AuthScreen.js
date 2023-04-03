f4647a112f46984957c0cddff0ca594a80b1db64
shvelo012 <shvelidzesaba09@gmail.com>
2023-04-03 17:24:39 +0400

    added demo login page elements

--- navigation/screens/Auth/AuthScreen.js
+++ navigation/screens/Auth/AuthScreen.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 export default function Auth({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogIn = () => {
     navigation.reset({
@@ -12,11 +15,24 @@ export default function Auth({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput
+        style={{ height: 40, width: 200 , borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={email => setEmail(email)}
+        value={email}
+        placeholder='Email'
+      />
+      <TextInput
+        style={{ height: 40, width: 200 , borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={password => setPassword(password)}
+        value={password}
+        placeholder='Password'
+        secureTextEntry={true}
+      />
+      
       <Text
-        onPress={handleLogIn}
-        // onPress={() => navigation.navigate('Dashboard')}
+        onPress={password? handleLogIn : null}
         style={{ fontSize: 26, fontWeight: 'bold' }}>Log in</Text>
-      <Text>Auth Screen</Text>
+      {/* <Text>Auth Screen</Text> */}
     </View>
   );
 }
