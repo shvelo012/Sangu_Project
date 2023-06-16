b4b760fbaddc9f7cf760aad75c92fd5bc2412cde
shvelo012 <shvelidzesaba09@gmail.com>
2023-06-16 13:34:07 +0400

    added headers

--- src/navigation/screens/Profile/ProfileScreen.js
+++ src/navigation/screens/Profile/ProfileScreen.js
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Row from '../../../components/Row/Row';
 import { styles } from './ProfileScreen.style';
 import { useState } from 'react';
+import Header from '../../../components/Header/Header';
 
 
 
@@ -51,7 +52,7 @@ export default function Profile({ navigation }) {
   const infoData = {
     fullName: info.firstName + ' ' + info.lastName,
     // status: info.profiles[0]?.state,
-    sex: info.gender,
+    sex: info.gender === 'female' ? 'ქალი' : 'კაცი',
     Nationality: info.nationality,
     IDnum: info.personalNo,
     TelNum: info.phone,
@@ -62,37 +63,41 @@ export default function Profile({ navigation }) {
 
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.container}>
-        <View style={styles.topContent}>
-          <Text style={styles.nameStyle}>{infoData.fullName}</Text>
-          <Text style={[styles.statusStyle, { color: statusColor }]}>{infoData.status}</Text>
+    <>
+      <Header onPress={() => navigation.navigate("insideDetails")} title={'პროფილი'} />
+
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+          <View style={styles.topContent}>
+            <Text style={styles.nameStyle}>{infoData.fullName}</Text>
+            <Text style={[styles.statusStyle, { color: statusColor }]}>{infoData.status}</Text>
+          </View>
+          <Row>
+            <Text style={styles.infoContentStyle}>სქესი</Text>
+            <Text style={styles.infoContentStyle}>{infoData.sex}</Text>
+          </Row>
+          <Row>
+            <Text style={styles.infoContentStyle}>ეროვნება</Text>
+            <Text style={styles.infoContentStyle}>{infoData.Nationality}</Text>
+          </Row>
+          <Row>
+            <Text style={styles.infoContentStyle}>პირადი ნომერი</Text>
+            <Text style={styles.infoContentStyle}>{infoData.IDnum}</Text>
+          </Row>
+          <Row>
+            <Text style={styles.infoContentStyle}>ტელ. ნომერი</Text>
+            <Text style={styles.infoContentStyle}>{infoData.TelNum}</Text>
+          </Row>
+          <Row>
+            <Text style={styles.infoContentStyle}>ელ-ფოსტა</Text>
+            <Text style={styles.infoContentStyle}>{infoData.Email}</Text>
+          </Row>
+          <Row style={styles.lastElement}>
+            <Text style={[styles.infoContentStyle, styles.lastContentStyle]}>ფაკულტეტი</Text>
+            <Text style={[styles.infoContentStyle, styles.lastContentStyle]}>{infoData.Faculty}</Text>
+          </Row>
         </View>
-        <Row>
-          <Text style={styles.infoContentStyle}>სქესი</Text>
-          <Text style={styles.infoContentStyle}>{infoData.sex}</Text>
-        </Row>
-        <Row>
-          <Text style={styles.infoContentStyle}>ეროვნება</Text>
-          <Text style={styles.infoContentStyle}>{infoData.Nationality}</Text>
-        </Row>
-        <Row>
-          <Text style={styles.infoContentStyle}>პირადი ნომერი</Text>
-          <Text style={styles.infoContentStyle}>{infoData.IDnum}</Text>
-        </Row>
-        <Row>
-          <Text style={styles.infoContentStyle}>ტელ. ნომერი</Text>
-          <Text style={styles.infoContentStyle}>{infoData.TelNum}</Text>
-        </Row>
-        <Row>
-          <Text style={styles.infoContentStyle}>ელ-ფოსტა</Text>
-          <Text style={styles.infoContentStyle}>{infoData.Email}</Text>
-        </Row>
-        <Row style={styles.lastElement}>
-          <Text style={[styles.infoContentStyle, styles.lastContentStyle]}>ფაკულტეტი</Text>
-          <Text style={[styles.infoContentStyle, styles.lastContentStyle]}>{infoData.Faculty}</Text>
-        </Row>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 }
